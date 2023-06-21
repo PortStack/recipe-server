cb7013fcd4be3c07bf027f70bb22f39f1cbf6aa8
pass0210 <pass0210@gmail.com>
2023-06-21 17:44:25 +0900

    검색 재재재 수정

--- src/main/java/com/teamz/recipe/config/SecurityConfig.java
+++ src/main/java/com/teamz/recipe/config/SecurityConfig.java
@@ -2,29 +2,20 @@ package com.teamz.recipe.config;
 
 import com.teamz.recipe.global.jwt.JwtAuthenticationFilter;
 import com.teamz.recipe.global.jwt.JwtProvider;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-
-import java.io.IOException;
 import java.util.List;
 
 @Configuration
